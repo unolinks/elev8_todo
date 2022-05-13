af18fff666bcd1c68f315c4c561d98f81f39b8ab
INNOCENT ONEMEGUKE <innocent.onemeguke@gmail.com>
2022-05-13 14:11:07 +0100

    my first commit

--- ELEV8_TODO/settings.py
+++ ELEV8_TODO/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku 
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,3 +128,6 @@ STATICFILES_DIRS = ['static']
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Heroku settings. 
+django_heroku.settings(locals())
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ELEV8_TODO.wsgi --log-file -
